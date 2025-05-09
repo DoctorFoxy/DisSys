0277cb9523b57038cec45b9d8ec7bdd80bec535a
OlZe <oliverzeidler97@gmail.com>
2025-05-09 12:39:59 +0200

    frontend: added item quantities for products of both suppliers, updated mock data

--- Assignment 2/frontend/Hangover-Cure-Frontend/src/app/models/item.model.ts
+++ Assignment 2/frontend/Hangover-Cure-Frontend/src/app/models/item.model.ts
@@ -1,7 +1,9 @@
 export interface Item {
     id: string;
     itemid1: string;
+    itemqty1: number;
     itemid2: string;
+    itemqty2: number;
     price: number;
     title: string;
     description: string;
--- Assignment 2/frontend/Hangover-Cure-Frontend/src/app/services/item.service.ts
+++ Assignment 2/frontend/Hangover-Cure-Frontend/src/app/services/item.service.ts
@@ -11,7 +11,9 @@ export class ItemService {
             {
                 id: '0',
                 itemid1: 'asadf',
+                itemqty1: 1,
                 itemid2: '1234',
+                itemqty2: 1,
                 title: 'Basic Hangover Cure',
                 description: 'Cheap and effective: Our most popular product for general cases. Featuring an electrolyte mix as well as some fresh fruit to get you back on your feet.',
                 imagelink: '',
@@ -20,7 +22,9 @@ export class ItemService {
             {
                 id: '1',
                 itemid1: 'anghff',
+                itemqty1: 1,
                 itemid2: '1535',
+                itemqty2: 1,
                 title: 'Light Brainfog Cure',
                 description: 'For those nights where you really only had a couple beers and now want to give you a tiny oomph: A small electrolyte mix and a banana.',
                 imagelink: '',
@@ -28,8 +32,10 @@ export class ItemService {
             },
             {
                 id: '2',
-                itemid1: 'oiuzhgf',
+                itemid1: 'asadf',
+                itemqty1: 2,
                 itemid2: '8587',
+                itemqty2: 1,
                 title: 'Dehydration Cure',
                 description: 'For when you have neglected your hydration: 2x electrolyte mix with 2L of orange juice. Get ready to start peeing!',
                 imagelink: '',
@@ -37,8 +43,10 @@ export class ItemService {
             },
             {
                 id: '3',
-                itemid1: 'iihki',
+                itemid1: 'asadf',
+                itemqty1: 1,
                 itemid2: '57782',
+                itemqty2: 1,
                 title: 'Productivity Boost',
                 description: 'When your alarm rings at 6am for work, you feel the brainfog and just stare in the mirror, scared of your performance at work: This is the cure! Comes with an electrolyte mix and a six pack of energy drinks.',
                 imagelink: '',
@@ -47,16 +55,20 @@ export class ItemService {
             {
                 id: '4',
                 itemid1: 'sadf',
+                itemqty1: 1,
                 itemid2: '5422',
+                itemqty2: 1,
                 title: 'Smoker\'s Cure',
-                description: 'Can\'t control yourself around those pesky cigarettes? Can you feel like your lungs struggling for air and making weird noises? Comes with an electrolyte mix, small asthma inhaler, and high peppermint bubblegum',
+                description: 'Can\'t control yourself around those pesky cigarettes? Can you feel like your lungs struggling for air and making weird noises? Comes with a small asthma inhaler, and high peppermint bubblegum',
                 imagelink: '',
                 price: 25.0
             },
             {
                 id: '5',
                 itemid1: 'asgijf',
+                itemqty1: 1,
                 itemid2: '1684',
+                itemqty2: 1,
                 title: 'Stuck in bed package',
                 description: 'Your head hurts and you can\'t get out of bed because everything in the world is hostile to your senses right now? We understand, you need to refuel your body with some precious fast food: Comes with an electrolyte mix, one ibuprofen and a juicy Döner Kebab.',
                 imagelink: '',
@@ -65,9 +77,11 @@ export class ItemService {
             {
                 id: '6',
                 itemid1: 'afwgw',
+                itemqty1: 1,
                 itemid2: '16886',
+                itemqty2: 1,
                 title: 'My body is on the brink of death package',
-                description: 'Title says it all. 2x electrolyte mix, 1x extra strong ibuprofen, 1x Pizza, 1x Dönner, 1x Beer to counter the hangover and 2L of orange juice to hydrate.',
+                description: 'Title says it all. 2x electrolyte mix, 1x extra strong ibuprofen, 1x Pizza, 1x Döner, 1x Beer to counter the hangover and 2L of orange juice to hydrate.',
                 imagelink: '',
                 price: 50.0
             },
