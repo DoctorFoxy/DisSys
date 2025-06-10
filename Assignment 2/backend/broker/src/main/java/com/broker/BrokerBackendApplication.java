b2065f0ec2144b5c16ef1b81786705f7af2351b3
OlZe <oliverzeidler97@gmail.com>
2025-06-10 14:56:41 +0200

    backend: add global CORS config for all paths to support frontend development

--- Assignment 2/backend/broker/src/main/java/com/broker/BrokerBackendApplication.java
+++ Assignment 2/backend/broker/src/main/java/com/broker/BrokerBackendApplication.java
@@ -2,6 +2,9 @@ package com.broker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BrokerBackendApplication {
@@ -12,4 +15,13 @@ public class BrokerBackendApplication {
 		SpringApplication.run(BrokerBackendApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200"); // For frontend development
+			}
+		};
+	}
 }
