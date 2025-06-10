90478475ad378d42c527afd94c39395c16d63f8c
OlZe <oliverzeidler97@gmail.com>
2025-06-10 16:04:24 +0200

    frontend: now include authorization token to API requests

--- Assignment 2/frontend/Hangover-Cure-Frontend/src/app/app.config.ts
+++ Assignment 2/frontend/Hangover-Cure-Frontend/src/app/app.config.ts
@@ -1,22 +1,38 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAuth0 } from '@auth0/auth0-angular';
+import { authHttpInterceptorFn, provideAuth0 } from '@auth0/auth0-angular';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+
     provideRouter(routes),
-    provideHttpClient(),
+
     provideAuth0({
       domain: 'dev-6vt2svtrmb7h7xy2.us.auth0.com',
       clientId: 'R7ebcX3OHzCwNNvhFfVQjZbG6EhrKpET',
       authorizationParams: {
         redirect_uri: window.location.origin,
-        audience: 'Backend'
+        audience: 'Backend',
+        scope: 'openid profile read:orders'
+      },
+      httpInterceptor: {
+        allowedList: [
+          {
+            // Allow un-authenticated calls to /api/items
+            uri: 'http://localhost:8080/api/items',
+            allowAnonymous: true
+          },
+
+          // Attach auth token to all other API calls
+          'http://localhost:8080/api/*',
+        ]
       }
     }),
+
+    provideHttpClient(withInterceptors([authHttpInterceptorFn])),
   ]
 };
